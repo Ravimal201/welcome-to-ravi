516f7bf4c405524b0ab067c207b587f82b1ef4eb
Sachinthaka Ravimal <universityravimal@gmail.com>
2026-04-01 00:46:46 +0530

    Create navbar and cover page with responsive

--- src/views/Navbar.jsx
+++ src/views/Navbar.jsx
@@ -1,26 +1,34 @@
+import { useState } from "react";
+
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     const handleMouseMove = (e) => {
         const item = e.currentTarget;
         const rect = item.getBoundingClientRect();
 
-        item.style.setProperty(
-            "--x",
-            `${e.clientX - rect.left}px`
-        );
-        item.style.setProperty(
-            "--y",
-            `${e.clientY - rect.top}px`
-        );
+        item.style.setProperty("--x", `${e.clientX - rect.left}px`);
+        item.style.setProperty("--y", `${e.clientY - rect.top}px`);
     };
-
 
     return (
         <nav className="navbar">
             <div className="logo">
-                <h1>&lt;/&gt; </h1>
+                <h1>&lt;/&gt;</h1>
                 <h2>Sachinthaka Ravimal</h2>
             </div>
-            <ul>
+
+            {/* HAMBURGER */}
+            <div 
+                className={`hamburger ${menuOpen ? "active" : ""}`}
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+
+            <ul className={menuOpen ? "nav-links active" : "nav-links"}>
                 <li onMouseMove={handleMouseMove}>Home</li>
                 <li onMouseMove={handleMouseMove}>About</li>
                 <li onMouseMove={handleMouseMove}>Projects</li>
